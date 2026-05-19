6aa0e0b596e581ed8d2df09b87bd9b89c564f50e
Boopathiraj94 <boopathirajk55@gmail.com>
2026-05-19 13:03:14 +0530

    array (string,number, str+nu,array of object,type , function with , without return ,optional , default ,task )

--- Naveentypscript/index.js
+++ Naveentypscript/index.js
@@ -1,18 +1,100 @@
-console.log("welcome");
-let studentname = "Subash";
-studentname = "naveen";
-let age = 20;
-let isPassExam = true;
-console.log(studentname, age, isPassExam);
-let selectedCow = null;
-selectedCow = "raja";
-console.log(selectedCow);
-let input = "Boopathi";
-if (typeof input === "string") {
-    console.log(input.toUpperCase());
+// console.log("welcome");
+// let studentname:string = "Subash"
+// studentname = "naveen"
+// let age:number = 20
+// let isPassExam:boolean = true
+// console.log(studentname , age , isPassExam);
+// let selectedCow:string | null = null
+// selectedCow = "raja"
+// console.log(selectedCow);
+// let input: unknown = "Boopathi";
+// if (typeof input === "string") {
+//   console.log(input.toUpperCase());
+// }
+// function printMessage(): void {
+//   console.log("Hello");
+// }
+// let nums = [10,40,'naveen','raja']
+// let marks:number[] = [20,40,60,80,100]
+// let studentnames : string[] = ['naveen','raja','kamalesh','bhatsha']
+// console.log(studentnames);
+// console.log(marks);
+// let student_marks:[string,number] = ['naveen',40]
+// console.log(student_marks);
+/*
+array of object
+
+[{},{},{},{}]
+*/
+// let employeelist: { name:string,age:number,salary:number,role:string }[] = [
+//   {
+//     name:'naveen',
+//     age:22,
+//     salary:200000,
+//     role:'developer'
+//   },
+//   {
+//     name:'raja',
+//     age:23,
+//     salary:40000,
+//     role:"tester"
+//   }
+// ]
+// console.log(employeelist);
+// // using Type alis
+// type Employee = { name:string,age:number,salary:number,role:string }
+// let emplist:Employee[] = []
+// emplist[0] =  {
+//     name:'raja',
+//     age:23,
+//     salary:40000,
+//     role:"tester"
+//   }
+// emplist[1] =  {
+//     name:'naveen',
+//     age:23,
+//     salary:40000,
+//     role:"tester"
+//   }
+// console.log(emplist);
+/*
+
+function functionName(){
+  //block of code
 }
-function printMessage() {
-    console.log("Hello");
-}
- 
+
+functionName() // calling / revoke
+
+*/
+// without return function
+// function addition(a: number, b: number) {
+//   console.log(a+b);
+// }
+// addition(20, 30)
+// // with return funtcion
+// function substraction(i:number,j:number):number {
+//   // console.log("inside function=> ,", i-j);
+//   let ans = (i-j)
+//   return ans; //explicit
+// }
+// let op = substraction(30,20);
+// console.log("explicit output => ",op);
+// let a:undefined = undefined;
+// optional parameter
+// function profile(name?:string | number){
+//   console.log("profile name",name);
+// }
+// profile()
+// profile("naveen")
+// profile(20)
+// default parameter function
+let add = (a = 0, b = 0) => {
+    debugger;
+    console.log(a + b);
+};
+// NaN => Not a Number
+add(10, 20); // 30
+add(10); // 10
+add(); // 0
+export {};
 //# sourceMappingURL=index.js.map
